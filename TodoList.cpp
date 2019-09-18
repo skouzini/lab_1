254a46431c1f5a4067c7cd969b06196705084e2c
Josh Skousen <skousenjosh@gmail.com>
2019-09-18 20:10:10 +0000

    Added and modified files

---
+++ TodoList.cpp
@@ -0,0 +1,38 @@
+#include "TodoList.h"
+
+TodoList::TodoList() {
+    
+}
+TodoList::~TodoList() {
+    
+}
+
+/*
+*   Adds an item to the todo list with the data specified by the string "_duedate" and the task specified by "_task"
+*/
+void TodoList::add(string _duedate, string _task) {
+    
+}
+
+/*
+*   Removes an item from the todo list with the specified task name
+*
+*   Returns 1 if it removes an item, 0 otherwise
+*/
+int TodoList::remove(string _task) {
+    
+}
+
+/*
+*   Prints out the full todo list to the console
+*/
+void TodoList::printTodoList() {
+    
+}
+
+/*
+*   Prints out all items of a todo list with a particular due date (specified by _duedate)
+*/
+void TodoList::printDaysTasks(string _date) {
+    
+}
